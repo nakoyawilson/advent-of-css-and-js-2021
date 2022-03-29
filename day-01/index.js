c200a62a796069a58adff2ec79624108dd0084ae
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-03-29 18:52:18 -0400

    Add edit time functionality

--- day-01/index.js
+++ day-01/index.js
@@ -1,6 +1,8 @@
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 const startStop = document.getElementById("start-stop");
+const edit = document.getElementById("edit");
+const editIcon = document.getElementById("edit-icon");
 const initialTime = Number(minutes.innerHTML) * 60 + Number(seconds.innerHTML);
 
 let time = initialTime;
@@ -34,5 +36,30 @@ startStop.addEventListener("click", () => {
   } else if (startStop.innerHTML === "Stop") {
     stopTimer();
     startStop.innerHTML = "Start";
+  }
+});
+
+edit.addEventListener("click", () => {
+  if (editIcon.src.includes("gear")) {
+    editIcon.src = "/assets/images/check.svg";
+    minutes.contentEditable = true;
+    seconds.contentEditable = true;
+    minutes.classList.add("edit-time");
+    seconds.classList.add("edit-time");
+    startStop.classList.add("hide-button");
+    startStop.disabled = true;
+  } else if (
+    editIcon.src.includes("check") &&
+    Number(minutes.innerHTML) < 60 &&
+    Number(seconds.innerHTML) < 60
+  ) {
+    editIcon.src = "/assets/images/gear.svg";
+    minutes.contentEditable = false;
+    seconds.contentEditable = false;
+    minutes.classList.remove("edit-time");
+    seconds.classList.remove("edit-time");
+    startStop.classList.remove("hide-button");
+    startStop.disabled = false;
+    time = Number(minutes.innerHTML) * 60 + Number(seconds.innerHTML);
   }
 });
